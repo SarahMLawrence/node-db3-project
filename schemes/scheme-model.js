937378e1aeefa3f50f978248be25d30b6649d6b6
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-08-24 23:05:24 -0400

    initial commit

---
+++ schemes/scheme-model.js
@@ -0,0 +1,30 @@
+const db = require("../data/config");
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
+
+function findSteps(id) {
+  return db("steps as st")
+    .join("schemes as sc", "sc.id", "st.scheme_id")
+    .select("sc.scheme_name", "st.step_number", "st.instructions")
+    .where({ scheme_id: id });
+}
+
+function add(scheme){
+    return db('schemes').insert(scheme)
+    .then(ids => {
+        return findById(ids[0]);
+    });
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const SchemeRouter = require('./schemes/scheme-router.js');
+const welcomeRouter = require('./welcome/welcome-router');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+
+server.use(welcomeRouter)
 
 module.exports = server;
